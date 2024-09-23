ef96524b21652daf0861419c0d6e68836d1b7e45
Bilal Patel <bilalpatel@outlook.com>
2024-09-22 23:58:31 -0400

    Create assignShift function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -41,3 +41,23 @@ const displayEmployeeShifts = employee => {
     });
 }
 // displayEmployeeShifts(steve); REMOVE // to test
+
+//Task 3 - Create a Function to Assign a New Shift
+
+const assignShift = (employeeName, day, hours) => {
+    const employee = employees.find(e => e.name === employeeName); // find employee by their name
+
+    // if not found
+    if (!employee) return;
+
+    // check if employee has shift that day
+    const hasShift = employee.shifts.some(shift => shift.day === day);
+
+    if (hasShift) {
+        console.log(`${employeeName} has a shift on ${day}`);
+    } else {// add new shift if one doesnt have a shift
+    employee.shifts.push({ day: day, hours: hours});
+    console.log(`adding shift on ${day} for ${employeeName}.`);
+}
+
+assignShift('Kyle', 'Saturday', 8);
